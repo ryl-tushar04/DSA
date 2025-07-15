aa8017b81484295871db552b64a0a8a7e6300fee
Tushar_irl <117632608+ryl-tushar04@users.noreply.github.com>
2025-07-15 23:12:44 +0530

    Time: 1 ms (99.12%) | Memory: 42 MB (80.13%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -1,13 +1,25 @@
 class Solution {
     public boolean isValid(String word) {
-       return word.length() >= 3 && word.chars().allMatch(Character::isLetterOrDigit) &&
-        word.chars().anyMatch(c -> isVowel((char) c)) &&
-        word.chars().anyMatch(c -> isConsonant((char) c));
-  }
-  private boolean isVowel(char c) {
-    return "aeiouAEIOU".indexOf(c) != -1;
-  }
-  private boolean isConsonant(char c) {
-    return Character.isLetter(c) && !isVowel(c);
+       if(word.length() < 3) {
+            return false;
+        }
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        for(int i = 0; i < word.length(); i++) {
+            switch(word.charAt(i)) {
+                case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+                    hasVowel = true;
+                    break;
+                case 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z',
+                     'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z':
+                    hasConsonant = true;
+                    break;
+                case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return hasVowel && hasConsonant;
   }
 }
